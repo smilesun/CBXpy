12c21ee68eba857c53e0b9d739e3e40fc3f80aa6
smilesun <smilesun.east@gmail.com>
2025-12-18 14:27:50 +0100

    .

--- demo_cbx_polarized.py
+++ demo_cbx_polarized.py
@@ -1,33 +1,48 @@
 import numpy as np
 
-from cbx.objectives import snowflake
+from cbx.objectives import snowflake, Rastrigin
+
 from cbx.dynamics import PolarCBO
 from cbx.scheduler import multiply
 
 from cbx_visual import vis_cbx_dynamic_history
+from plot_snowflake import plot_objective
 
 
 np.random.seed(42)
-f = snowflake()
+
 N = 100
 x = np.random.uniform(-2., 2., (2, N, 2))
 
-dyn = PolarCBO(f,  # objective function
-               d=2,  # dimension of system,
-               # default None if initial particles are given
-               x=x,  # initial particles, will be reshaped according to d
-               alpha=1.,  # heat
-               # noise='anisotropic',
-               sigma=0.,  # noise
-               kappa=1.5,
-               verbosity=0,
-               track_args={'names': [
-                   'x',
-                   'consensus',
-                   'drift']},
-               batch_args={'size': 50})
 
-dyn.optimize(sched=multiply(factor=1.02, maximum=1e10))
+def create_demo(obj_func, initial_particles,
+                sigma,
+                extra_info=None):
+    name = obj_func.__class__.__name__
+
+    dyn = PolarCBO(obj_func,  # objective function
+                   d=2,  # dimension of system,
+                   # default None if initial particles are given
+                   x=x,  # initial particles, will be reshaped according to d
+                   alpha=1.,  # heat
+                   # noise='anisotropic',
+                   sigma=2.,  # noise
+                   kappa=1.5,
+                   verbosity=0,
+                   track_args={'names': [
+                       'x',
+                       'consensus',
+                       'drift']},
+                   batch_args={'size': 50})
+
+    dyn.optimize(sched=multiply(factor=1.02, maximum=1e10))
+
+    vis_cbx_dynamic_history(dyn, box=2,
+                            save_gif_path=f"cbo_{name}_sigma_{sigma}.gif")
 
 
-vis_cbx_dynamic_history(dyn, box=2, save_gif_path="polar_cbo_sigma0.gif")
+if __name__ == "__main__":
+    # create_demo(three_hump_camel(), x, "polarized_three_hump_camel")
+    create_demo(Rastrigin(), x, sigma=0)
+    # create_demo(snowflake(), x, "polarized_snowflake")
+    plot_objective(Rastrigin())
--- plot_snowflake.py
+++ plot_snowflake.py
@@ -1,31 +1,35 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
-from cbx.objectives import snowflake
-fig = plt.figure(figsize=(15,5))
-x_min = -2.5
-x_max =  2.5
-y_min = -2.5
-y_max =  2.5
-f = snowflake()
-
-num_pts_landscape = 100
-xx = np.linspace(x_min, x_max, num_pts_landscape)
-yy = np.linspace(y_min, y_max, num_pts_landscape)
-XX, YY = np.meshgrid(xx,yy)
-XXYY = np.stack((XX.T,YY.T)).T
-Z = np.zeros((num_pts_landscape,num_pts_landscape, 2))
-Z[:,:,0:2] = XXYY
-ZZ = f(Z)
-
-ax0 = fig.add_subplot(121)
-ax1 = fig.add_subplot(122, projection='3d')
-cs = ax0.contourf(XX,YY,ZZ, 20, cmap=cm.get_cmap('Blues'))
-ax0.contour(cs, colors='white', alpha=0.2)
-ax0.scatter(f.minima[:, 0], f.minima[:, 1], color='blue', marker='x', s=20)
-ax1.plot_surface(XX,YY,ZZ, cmap=cm.get_cmap('Blues'))
-ax0.set_title('Contour plot')
-ax1.set_title('Surface plot')
-plt.show()
 
 
+def plot_objective(obj_func, name=None, box=2.5, num_pts_landscape=100):
+    if name is None:
+        name = obj_func.__class__.__name__
+    fig = plt.figure(figsize=(15, 5))
+    x_min = -box
+    x_max = box
+    y_min = -box
+    y_max = box
+    xx = np.linspace(x_min, x_max, num_pts_landscape)
+    yy = np.linspace(y_min, y_max, num_pts_landscape)
+    XX, YY = np.meshgrid(xx, yy)
+    XXYY = np.stack((XX.T, YY.T)).T
+    Z = np.zeros((num_pts_landscape, num_pts_landscape, 2))
+    Z[:, :, 0:2] = XXYY
+    ZZ = obj_func(Z)
+
+    ax0 = fig.add_subplot(121)
+    ax1 = fig.add_subplot(122, projection='3d')
+    cs = ax0.contourf(XX, YY, ZZ, 20, cmap=cm.get_cmap('Blues'))
+
+    ax0.contour(cs, colors='white', alpha=0.2)
+    if hasattr(obj_func, 'minima'):
+        ax0.scatter(obj_func.minima[:, 0], obj_func.minima[:, 1],
+                    color='blue', marker='x', s=20)
+
+    ax1.plot_surface(XX, YY, ZZ, cmap=cm.get_cmap('Blues'))
+    ax0.set_title('Contour plot')
+    ax1.set_title('Surface plot')
+    plt.savefig(f'{name}_objective.pdf')
+    plt.show()
